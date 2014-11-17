dac55300a7ff82b54472e848665dc2c25ccf4d54
sayapapaya <sayapapaya@users.noreply.github.com>
2014-11-17 14:24:04 -0500

    Update edrum.ino

--- edrum.ino
+++ edrum.ino
@@ -1,8 +1,8 @@
-// Saya Date and Eddie Farias
+// Saya Date,Eddie Farias & Stephen Suen
 // 2.009
 // Tech Review
 // eDrum
-// November 11, 2014
+// November 17, 2014
 
 // intializing drum LED output
 const int Crash_R = 22;
@@ -288,6 +288,110 @@ void feedback(){
 ////////////////////////////////////////////
 ///// MODE 1: 
 
+void Learn_LR()
+{
+//  Serial.println("LEARN MODE");
+  for(int i = 0; i < length; i++)
+  {
+    // Start with no drums to be played
+    // using binary: 0 for do not play, 1 for play  
+    int hit_0 = 0;
+    int hit_1 = 0;
+    int hit_2 = 0;
+    
+//    Serial.print("  Drums to hit:");
+      
+    // light up correct drums to be played  
+    if(track1[i] > -1)
+    {
+      digitalWrite(LED_R[track1[i]], HIGH);
+      hit_0 = 1;
+//      Serial.print(" ");
+//      Serial.print(track1[i]);
+    }
+    
+    if(track2[i] > -1)
+    {
+      digitalWrite(LED_G[track2[i]], HIGH);
+      hit_1 = 1;
+//      Serial.print(" ");
+//      Serial.print(track2[i]);
+    }
+    
+    if(track3[i] > -1)
+    {
+      digitalWrite(LED_B[track3[i]], HIGH);
+      hit_2 = 1;
+//      Serial.print(" ");
+//      Serial.println(track3[i]);
+    }
+    
+    Serial.println("");
+    
+    // wait until all drums in sequence have been hit 
+    while(hit_0 == 1 || hit_1 == 1 || hit_2 == 1)
+    {
+      int start_time = millis() - time;
+      if(hit_0 == 1 && analogRead(sensors[track1[i]]) > threshold[track1[i]])
+      {
+        hit_0 = 0;
+        String output = "[h:" + String(track1[i]) + "," + String(start_time) + "]";
+//        Serial.print("    ");
+        Serial.println(output);
+      }
+        
+      if(hit_1 == 1 && analogRead(sensors[track2[i]]) > threshold[track2[i]])
+      {
+        hit_1 = 0;
+        String output = "[h:" + String(track2[i]) + "," + String(start_time) + "]";
+//        Serial.print("    ");
+        Serial.println(output);
+      }
+        
+      if(hit_2 == 1 && analogRead(sensors[track3[i]]) > threshold[track3[i]])
+      {
+        hit_2 = 0;
+        String output = "[h:" + String(track2[i]) + "," + String(start_time) + "]";
+//        Serial.print("    ");
+        Serial.println(output);
+      }   
+    }
+    
+    // turn off all drums 
+    digitalWrite(LED_G[0], LOW);
+    digitalWrite(LED_G[1], LOW);
+    digitalWrite(LED_G[2], LOW);
+    digitalWrite(LED_G[3], LOW);
+    digitalWrite(LED_G[4], LOW);
+    digitalWrite(LED_G[5], LOW);
+    digitalWrite(LED_G[6], LOW);
+    digitalWrite(LED_G[7], LOW);
+    
+    digitalWrite(LED_R[0], LOW);
+    digitalWrite(LED_R[1], LOW);
+    digitalWrite(LED_R[2], LOW);
+    digitalWrite(LED_R[3], LOW);
+    digitalWrite(LED_R[4], LOW);
+    digitalWrite(LED_R[5], LOW);
+    digitalWrite(LED_R[6], LOW);
+    digitalWrite(LED_R[7], LOW);
+    
+    digitalWrite(LED_B[0], LOW);
+    digitalWrite(LED_B[1], LOW);
+    digitalWrite(LED_B[2], LOW);
+    digitalWrite(LED_B[3], LOW);
+    digitalWrite(LED_B[4], LOW);
+    digitalWrite(LED_B[5], LOW);
+    digitalWrite(LED_B[6], LOW);
+    digitalWrite(LED_B[7], LOW);
+    
+    delay(500);
+  }  
+  
+  Serial.println("[e]");
+}
+////
+//Without right and left input
 void Learn()
 {
 //  Serial.println("LEARN MODE");
@@ -390,8 +494,102 @@ void Learn()
   
   Serial.println("[e]");
 }
-  
 ////////////////////////////////////////////
+///// DEMO with right left:
+void Demo_LR()
+{
+  boolean songEnd = false;
+  for(int i=0 ; i < length; i++ ){
+      int led_time = (lengths[i]/bpm)*60000;
+ //Turning on LEDs of corresponding drums
+      if (track1[i] > -1)
+      {
+        digitalWrite(LED_G[track1[i]], HIGH);
+      }
+      if(track2[i] > -1)
+      {
+        digitalWrite(LED_G[track2[i]], HIGH);
+      }
+      if(track3[i] > -1)
+      {
+        digitalWrite(LED_G[track3[i]], HIGH);
+      }
+  
+
+//    
+//    //How long LED Stays on
+      delay(led_time/2); //This delay would change based on size of note later intialized in an array
+//    
+//    //turning off LEDs
+      if (track1[i] > -1)
+      {
+        digitalWrite(LED_G[track1[i]], LOW);
+      }
+      if(track2[i] > -1)
+      {
+       digitalWrite(LED_G[track2[i]], LOW);
+      }
+      if(track3[i] > -1)
+     {
+       digitalWrite(LED_G[track3[i]], LOW);
+     }
+     delay(led_time/2); // this delay is part of the shared delay; delay between notes
+     if(i==length-1){
+       delay(3000);
+       songEnd == true;
+     }
+    }
+    //keep getting feedback till the song ends
+  }
+  
+  
+  /////////////////////////////////////////////////////
+  ////////////////// Demo without right left
+  void Demo()
+{
+  boolean songEnd = false;
+  for(int i=0 ; i < length; i++ ){
+      int led_time = (lengths[i]/bpm)*60000;
+ //Turning on LEDs of corresponding drums
+      if (track1[i] > -1)
+      {
+        digitalWrite(LED_G[track1[i]], HIGH);
+      }
+      if(track2[i] > -1)
+      {
+        digitalWrite(LED_G[track2[i]], HIGH);
+      }
+      if(track3[i] > -1)
+      {
+        digitalWrite(LED_G[track3[i]], HIGH);
+      }
+  
+
+//    
+//    //How long LED Stays on
+      delay(led_time/2); //This delay would change based on size of note later intialized in an array
+//    
+//    //turning off LEDs
+      if (track1[i] > -1)
+      {
+        digitalWrite(LED_G[track1[i]], LOW);
+      }
+      if(track2[i] > -1)
+      {
+       digitalWrite(LED_G[track2[i]], LOW);
+      }
+      if(track3[i] > -1)
+     {
+       digitalWrite(LED_G[track3[i]], LOW);
+     }
+     delay(led_time/2); // this delay is part of the shared delay; delay between notes
+     if(i==length-1){
+       delay(3000);
+       songEnd == true;
+     }
+    }
+  }
+
 ////////////////////////////////////////////
 ///// MODE 2: FOLLOW 
 ///// this mode currently does not allow for accuracy readings, can be implemented later on now
@@ -471,9 +669,21 @@ void loop(){
     time = millis();
     
     // Choose Mode
-    if (mode == 0){    
+    if (mode == 0){
+      if(action ==0){
+        Learn();
+      }
+      if(action ==1){
+        Learn_LR();
+      }
+      if(action ==2){
+        Demo();
+      }
+      if(action ==3){
+        Demo_LR();
+      }    
       // Wait to hit
-      Learn();
+      
     }
     if (mode == 1){
       // Play through
